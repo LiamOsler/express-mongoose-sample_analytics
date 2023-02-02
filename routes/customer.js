2c62592e86c347a7950ebcff0c4783961b0e4266
Liam Osler <osler.liam@gmail.com>
2023-02-02 18:41:23 -0400

    update rroutes to server JSON, enable cors

--- routes/customer.js
+++ routes/customer.js
@@ -8,7 +8,7 @@ var Model = require('../models/Customers');
 router.get('/', async function(req, res, next) {
   var result = Model.find({}, function (err, docs) {
     if(err)  console.error(err);
-    if(docs) res.send(docs);
+    if(docs) res.json(docs);
   });
 });
 
@@ -16,31 +16,47 @@ router.get('/', async function(req, res, next) {
 router.get('/:n(\\d+)/:k(\\d+)', async function(req, res, next) {
   var result = Model.find({}, function (err, docs) {
     if(err)  console.error(err);
-    if(docs) res.send(docs);
+    if(docs) res.json(docs);
   }).skip(parseInt(req.params.n)).limit(parseInt(req.params.k));
 });
 
 //Route to get customer usernames
 router.get('/username/', async function(req, res, next) {
   var result = Model.find({}, function (err, docs) {
-    if(err)  res.send(err);
-    if(docs) res.send(docs);
+    if(err)  res.json(err);
+    if(docs) res.json(docs);
   }).select('username');
 });
 
 //Route to get a customer by their specific username
 router.get('/username/:username', async function(req, res, next) {
   var result = Model.findOne({username:req.params.username}, function (err, docs) {
-    if(err)  res.send(err);
-    if(docs) res.send(docs);
+    if(err)  res.json(err);
+    if(docs) res.json(docs);
+  });
+});
+
+//Route to get customer usernames
+router.get('/email/', async function(req, res, next) {
+  var result = Model.find({}, function (err, docs) {
+    if(err)  res.json(err);
+    if(docs) res.json(docs);
+  }).select('email');
+});
+
+//Route to get a customer by their specific username
+router.get('/email/:email', async function(req, res, next) {
+  var result = Model.findOne({email:req.params.email}, function (err, docs) {
+    if(err)  res.json(err);
+    if(docs) res.json(docs);
   });
 });
 
 //Search for a user using their text index
 router.get('/search/:query', async function(req, res, next) {
   var result = Model.find({$text: {$search: req.params.query}}, function (err, docs) {
-    if(err)  res.send(err);
-    if(docs) res.send(docs);
+    if(err)  res.json(err);
+    if(docs) res.json(docs);
   });
 });
 
@@ -52,7 +68,7 @@ router.get('/count/', async function(req, res, next) {
         console.log(err);
     }else{
         console.log("Count :", count);
-        res.send({count:count});
+        res.json({count:count});
     }
   });
 });
